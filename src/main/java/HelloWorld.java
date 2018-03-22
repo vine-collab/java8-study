252927fc75d3c762c73602786a0565548a05f6a7
hekai <hekai0118@gmail.com>
2018-03-22 11:27:12 +0800

    HelloWorld

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * @author hekai
+ * @create 2018-03-22 上午11:26
+ */
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("hello java8!");
+    }
+
+}
