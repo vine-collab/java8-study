7950033de8aa8f0b2c29d084f1d42de9330b8c7c
hekai <hekai0118@gmail.com>
2018-04-08 19:48:45 +0800

    lambda

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,3 +1,8 @@
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 /**
  * @author hekai
  * @create 2018-03-22 上午11:26
@@ -7,6 +12,10 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("hello java8!");
+
+
+        List<Integer> collect = IntStream.rangeClosed(2, 100).boxed().collect(Collectors.toList());
+        System.out.println(collect);
     }
 
 }
---
+++ src/main/java/com/blueberry/jdk8/Test2.java
@@ -0,0 +1,38 @@
+package com.blueberry.jdk8;
+
+/**
+ * @author hekai
+ * @create 2018-04-08 下午7:22
+ */
+@FunctionalInterface
+interface MyInterface {
+
+    void test();
+
+
+    @Override
+    String toString();
+}
+
+
+public class Test2{
+    public void myTest(MyInterface myInterface){
+        System.out.println(1);
+        myInterface.test();
+        System.out.println(2);
+    }
+
+    public static void main(String[] args) {
+        Test2 test2 = new Test2();
+        test2.myTest(new MyInterface() {
+            @Override
+            public void test() {
+                System.out.println("yyyy");
+            }
+        });
+
+        System.out.println("----------");
+
+        test2.myTest(() -> System.out.println("xxxx"));
+    }
+}
