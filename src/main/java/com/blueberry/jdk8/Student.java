7f76f879612feebd969898d21582612eecf528fb
hekai <hekai0118@gmail.com>
2018-04-10 19:54:52 +0800

    Supplier接口

---
+++ src/main/java/com/blueberry/jdk8/Student.java
@@ -0,0 +1,16 @@
+package com.blueberry.jdk8;
+
+import lombok.Data;
+
+/**
+ * @author hekai
+ * @create 2018-04-10 下午7:45
+ */
+@Data
+public class Student {
+
+    private String name = "Abby";
+
+    private int age;
+
+}
---
+++ src/main/java/com/blueberry/jdk8/SupplierTest.java
@@ -0,0 +1,17 @@
+package com.blueberry.jdk8;
+
+
+import java.util.function.Supplier;
+
+/**
+ * @author hekai
+ * @create 2018-04-10 下午7:44
+ */
+
+public class SupplierTest {
+
+    public static void main(String[] args) {
+        Supplier<String> supplier = () -> "xxxx";
+    }
+
+}
