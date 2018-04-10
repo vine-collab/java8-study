b6b7174e6d32aa7353a5c7ac60b52029af13ad37
hekai <hekai0118@gmail.com>
2018-04-10 17:06:49 +0800

    BiFunction 行为传递

---
+++ src/main/java/com/blueberry/jdk8/Person.java
@@ -0,0 +1,19 @@
+package com.blueberry.jdk8;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @author hekai
+ * @create 2018-04-10 下午3:48
+ */
+@Data
+@AllArgsConstructor
+public class Person {
+
+    private String userName;
+
+    private int age;
+
+
+}
