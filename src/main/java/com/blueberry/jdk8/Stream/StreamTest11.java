30325999fae74e8e129a7b7a4fe5d21cf1336774
hekai <hekai0118@gmail.com>
2018-04-15 21:18:39 +0800

    groupingby&partitioningBy

---
+++ src/main/java/com/blueberry/jdk8/Stream/StreamTest11.java
@@ -0,0 +1,26 @@
+package com.blueberry.jdk8.Stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author Alman
+ * @date 2018-04-15 下午5:31
+ */
+
+public class StreamTest11 {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("hello world hello", "world hello", "hello world");
+
+        list.stream().mapToInt(item -> {
+            System.out.println(item);
+            return item.length();
+        }).filter(length -> length >= 5).findFirst().ifPresent(System.out::println);
+
+
+        List<String> collect = list.stream().map(item -> item.split(" ")).flatMap(Arrays::stream).distinct().collect(Collectors.toList());
+        System.out.println(collect);
+
+    }
+}
---
+++ src/main/java/com/blueberry/jdk8/Stream/Student.java
@@ -0,0 +1,20 @@
+package com.blueberry.jdk8.Stream;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @author hekai
+ * @create 2018-04-11 下午2:39
+ */
+@Data
+@AllArgsConstructor
+public class Student {
+
+    private String name;
+
+    private int score;
+
+    private int age;
+    
+}
